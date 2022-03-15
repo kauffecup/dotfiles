1211f8c7598c5cd28e37eecaac98b03ba7f81a1e
Jonathan Kaufman <jkaufman@squarespace.com>
2022-03-15 16:44:53 -0400

    Make null-ls eslint finding more robust

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,20 +1,36 @@
-local nls = require('null-ls')
-local helpers = require('null-ls.helpers')
+local nls = require("null-ls")
+require("null-ls.helpers")
+local lsputil = require("lspconfig.util")
 
-local function eslint_condition(utils)
-  local has_js_config = utils.root_has_file('.eslintrc.js') or utils.root_has_file('.eslintrc.cjs')
-  local has_yaml_config = utils.root_has_file('.eslintrc.yaml') or utils.root_has_file('.eslintrc.yml')
-  local has_json_config = utils.root_has_file('.eslintrc.json')
-  return has_js_config or has_yaml_config or has_json_config
+local function check_dir_for_eslint(dir)
+  return (
+    lsputil.path.exists(lsputil.path.join(dir, ".eslintrc"))
+    or lsputil.path.exists(lsputil.path.join(dir, ".eslintrc.js"))
+    or lsputil.path.exists(lsputil.path.join(dir, ".eslintrc.cjs"))
+    or lsputil.path.exists(lsputil.path.join(dir, ".eslintrc.yaml"))
+    or lsputil.path.exists(lsputil.path.join(dir, ".eslintrc.yml"))
+    or lsputil.path.exists(lsputil.path.join(dir, ".eslintrc.json"))
+  )
 end
 
-require("null-ls").setup({
+local function eslint_condition()
+  local filepath = vim.fn.fnamemodify(vim.fn.expand("%"), ":p:h")
+  return check_dir_for_eslint(filepath) or lsputil.path.traverse_parents(filepath, check_dir_for_eslint)
+end
+
+local diagnostics = nls.builtins.diagnostics
+local formatting = nls.builtins.formatting
+
+nls.setup({
   sources = {
-    nls.builtins.diagnostics.eslint_d.with({
-      condition = eslint_condition
-    }),
-    nls.builtins.formatting.eslint_d.with({
-      condition = eslint_condition
-    }),
-  }
+    diagnostics.eslint_d.with({ runtime_condition = eslint_condition }),
+    formatting.eslint_d.with({ runtime_condition = eslint_condition }),
+    formatting.prettier.with({ runtime_condition = function() return vim.fn.mode() == 'V' end})
+  },
+  root_dir = function(fname)
+    return lsputil.path.traverse_parents(fname, function(dir)
+      return lsputil.path.exists(dir .. '/package.json') and (dir .. '/package.json')
+    end)
+  end,
+  debug = true
 })
