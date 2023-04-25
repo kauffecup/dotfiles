dae29c498fe2b3f155e856bd6dfe4ca042a773a7
Jonathan Kaufman <jondakauf@gmail.com>
2023-04-25 09:10:21 -0700

    fix multiple different client offset encodings

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -52,6 +52,12 @@ formatters.setup {
     },
 }
 
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+local capabilities = require("lvim.lsp").common_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+local opts = {capabilities = capabilities}
+require("lvim.lsp.manager").setup("clangd", opts)
+
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
