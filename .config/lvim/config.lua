17b8e687395a0a0f90cd8a4e3308c021e9d46c8f
Jonathan Kaufman <jondakauf@gmail.com>
2023-05-10 11:35:32 -0400

    better copilot config & update plugins

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -17,19 +17,20 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "cpp",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "html",
+  "java",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "rust",
+  "typescript",
+  "tsx",
+  "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -38,39 +39,39 @@ lvim.builtin.treesitter.highlight.enable = true
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-    { command = "clang-format", filetypes = { "cpp" } },
-    {
-        command = "prettier",
-        filetypes = {
-            "javascriptreact",
-            "javascript",
-            "typescript",
-            "typescriptreact",
-            "css",
-            "ruby"
-        }
-    },
+  { command = "clang-format", filetypes = { "cpp" } },
+  {
+    command = "prettier",
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescript",
+      "typescriptreact",
+      "css",
+      "ruby",
+    }
+  },
 }
 
 -- set clangd formatter to use utf-16 to avoid multiple encoding warning
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 local capabilities = require("lvim.lsp").common_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
-local opts = {capabilities = capabilities}
+local opts = { capabilities = capabilities }
 require("lvim.lsp.manager").setup("clangd", opts)
 
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-    {
-        command = "eslint_d",
-        filetypes = {
-            "javascriptreact",
-            "javascript",
-            "typescript",
-            "typescriptreact"
-        }
+  {
+    command = "eslint_d",
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescript",
+      "typescriptreact"
     }
+  }
 }
 
 -- Plugin Configuration
@@ -90,40 +91,45 @@ lvim.builtin.project.exclude_dirs = { "~/figma/figma/*" }
 
 -- Additional Plugins
 lvim.plugins = {
-    { "zbirenbaum/copilot.lua" },
-    {
-        "zbirenbaum/copilot-cmp",
-        event = "InsertEnter",
-        dependencies = { "zbirenbaum/copilot.lua" },
-        config = function()
-          vim.defer_fn(function()
-            require("copilot").setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            }) -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-            require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-          end, 100)
-        end,
-    },
-    -- {
-    --   "zbirenbaum/copilot-cmp",
-    --   after = { "copilot.lua" },
-    --   config = function()
-    --     require("copilot_cmp").setup()
-    --   end
-    -- },
-    {
-        "folke/trouble.nvim",
-        cmd = "TroubleToggle",
-    },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
 }
 
+-- Copilot config
+local ok, copilot = pcall(require, "copilot")
+if ok then
+  copilot.setup {
+    suggestion = {
+      auto_trigger = true,
+      keymap = {
+        accept = "<c-l>",
+        next = "<c-j>",
+        prev = "<c-k>",
+        dismiss = "<c-h>",
+      },
+    },
+  }
+
+  -- ctrl+s to toggle on/off copilot auto suggestions (when off can still trigger via 'next' cmd)
+  local copilotOpts = { noremap = true, silent = true }
+  vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", copilotOpts)
+end
+
+
+-- Add trouble to which key
 lvim.builtin.which_key.mappings["t"] = {
-    name = "+Trouble",
-    r = { "<cmd>Trouble lsp_references<cr>", "References" },
-    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-    d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-    w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 }
