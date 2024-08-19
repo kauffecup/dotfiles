18c050a7b352dd6ef94da595806cc8e0fb34fa73
Jonathan Kaufman <jondakauf@gmail.com>
2024-08-19 09:58:25 -0400

    vtsls-->tsserver, nvim-tree-->oil

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -14,6 +14,9 @@ return {
         clangd = function(_, opts)
           opts.capabilities.offsetEncoding = { "utf-16" }
         end,
+        typescript = function(_, opts)
+          opts.tsserver.log = "verbose"
+        end,
       },
     },
   },
@@ -35,6 +38,7 @@ return {
         { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
         { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
         { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+        { action = "Oil",                                            desc = " File Explorer",   icon = " ", key = "-" },
         { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
       }
 
@@ -45,7 +49,62 @@ return {
     end,
   },
 
+  {
+    "stevearc/oil.nvim",
+    cmd = "Oil",
+    opts = {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(path)
+          return path == ".." or path == ".git"
+        end,
+      },
+      window_options = {
+        wrap = true,
+      },
+      -- remap keys so we can have consistency for splits and pane navigation
+      use_default_keymaps = false,
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<leader>|"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        ["<leader>-"] = {
+          "actions.select",
+          opts = { horizontal = true },
+          desc = "Open the entry in a horizontal split",
+        },
+        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+        ["<C-p>"] = "actions.preview",
+        ["q"] = "actions.close",
+        ["<C-r>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    },
+    keys = {
+      {
+        "-",
+        function()
+          require("oil").open()
+        end,
+        desc = "File explorer",
+      },
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  },
+
   -- exclude
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
